879e1bea37e0efc48a7bf43e83567366709b41d1
RamonQuerol <ramon.querol.leon@gmail.com>
2025-04-12 22:41:55 +0200

    Added the basic commands to the script

---
+++ sla.sh
@@ -0,0 +1,76 @@
+
+#!/bin/bash
+
+SLA_DIR="$(realpath $(dirname "$0"))"
+
+# $1 Command name, $2 Script launcher name
+is_file_registered(){
+    if [ ! -f "$SLA_DIR/launchers/$2" ]; then
+        echo "The command $2 is not registered, so you can not apply the command $1 to it."
+        exit 1
+    fi
+}
+
+# $1 = NumArgs, $2 = Expected
+check_num_arguments(){
+    if [ $1 -ne $2 ]; then
+        echo "You did not give enough arguments, you gave $1 instead of $2 use \"sla help\" for more information"
+        exit 1
+    fi
+}
+
+case $1 in
+    
+    run)
+        check_num_arguments $# 2
+        is_file_registered run $2
+        exec "$SLA_DIR/launchers/$2"
+        ;;
+    add)
+        check_num_arguments $# 3
+        if [ ! -f "$2" ]; then
+            echo Could not find the script $2 in your system, check if that is the exact name of the file you want to add.
+            exit 2
+        fi
+        
+        if [ ! -d "$SLA_DIR/scripts" ]; then
+            mkdir "$SLA_DIR/scripts"
+        fi
+
+        SCRIPT_NAME=$(basename $2)
+        SCRIPT_LOC="$SLA_DIR/scripts/$SCRIPT_NAME"
+        cp $2 $SCRIPT_LOC
+
+        if [ ! -d "$SLA_DIR/launchers" ]; then
+            mkdir "$SLA_DIR/launchers"
+        fi
+
+        LAUNCHER_FILE="$SLA_DIR/launchers/$(echo $SCRIPT_NAME | cut -f 1 -d ".")"
+        LAUNCHER_SCRIPT="$(echo $3 | sed "s|\.\.\.|$SCRIPT_LOC|g")"
+        echo "#$SCRIPT_NAME" > $LAUNCHER_FILE
+        echo $LAUNCHER_SCRIPT >> $LAUNCHER_FILE
+        chmod +x $LAUNCHER_FILE
+        
+        echo "$2 has been registered into the sla database"
+        ;;
+    del)
+        check_num_arguments $# 2
+        is_file_registered del $2
+        SCRIPT_NAME_COMMENT="$(head -n 1 "$SLA_DIR/launchers/$2")"
+        SCRIPT_NAME="${SCRIPT_NAME_COMMENT:1}"
+
+        rm "$SLA_DIR/scripts/$SCRIPT_NAME"
+        rm "$SLA_DIR/launchers/$2"
+        
+        echo "$2 has been deleted from sla database"
+        ;;
+    help)
+
+        ;;
+    "")
+        echo "You have to add the command, use sla help to know the available commands"
+        ;;
+    *)
+        echo "$1 is not a valid command, use the command help to know the available commands"
+        ;;
+esac
